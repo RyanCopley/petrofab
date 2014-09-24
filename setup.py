7c0e40e60de7ec598fcc236d6a7778cb6c2614b3
Petr Zelenin <po.zelenin@yandex.ru>
2014-09-24 14:29:27 +0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# native
+import os
+from setuptools import find_packages, setup
+
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='petrofab',
+    version='0.0.1',
+    author='Petr Zelenin',
+    author_email='po.zelenin@gmail.com',
+    license='BSD',
+    description='Tasks based on Fabric for work with my project',
+    packages=find_packages(),
+    package_data={'petrofab': ['defaults.json']},
+    # long_description=read('README'),
+    setup_requires=[
+        'fabric',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
