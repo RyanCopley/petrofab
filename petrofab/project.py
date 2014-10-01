d34814ae9546764cfed4c0d26bcf4dc038039d1e
Petr Zelenin <po.zelenin@yandex.ru>
2014-10-01 12:11:12 +0600

    Some fixes

--- petrofab/project.py
+++ petrofab/project.py
@@ -27,9 +27,9 @@ class Project(object):
         self.name = name
         self.path = path_abs(path)
         self.env = project
-        self.url = url
-        self.src = src
-        self.trash = trash or ['.git*', 'readme.*']
+        self.url = url or self.env.get('url', None)
+        self.src = src or self.env.get('src', None)
+        self.trash = trash or self.env.get('trash', ['.git*', 'readme.*'])
 
     def make_path(self):
         """Create root path of project."""
@@ -50,12 +50,10 @@ class Project(object):
         Args:
             url (str): URL to repo with project template.
         """
-        _path = self.path
-        _url = url or self.url or self.env.get('url', None)
-        _url = _url or prompt(u'URL to repo with template:')
-        self.url = _url
+        _url = url or self.url
+        self.url = _url or prompt(u'URL to repo with template:')
         with settings(warn_only=True):
-            result = run('git clone {0} {1}'.format(_url, _path))
+            result = run('git clone {0} {1}'.format(self.url, self.path))
             if result.failed:
                 msg = result
                 abort(red(msg.format(**self.__dict__)))
@@ -68,15 +66,13 @@ class Project(object):
             trash (list): list of patterns which will remove.
             silently (bool): signal for ask user about confirm operation.
         """
-        _path = self.path
-        _trash = trash or self.env.get('trash', []) or self.trash
-        self.trash = _trash
+        self.trash = trash or self.trash
         msg = yellow(u'Be careful! This operation will not cancel! Continue?')
         if not silently and not confirm(msg):
             print(red(u'Canceled by user!'))
             return
-        with cd(_path):
-            for pattern in _trash:
+        with cd(self.path):
+            for pattern in self.trash:
                 run('find . -iname "%s" -print0 | xargs -0 rm -rf' % pattern)
         print(green(u'"{path}" was clean.').format(**self.__dict__))
 
@@ -87,13 +83,11 @@ class Project(object):
             src (str): URL of repo with source code, defaults is None.
             path (str): Path to new project, defaults to '~/work'.
         """
-        _path = self.path
-        _path = os.path.join(_path, 'src')
-        _src = src or self.src or self.env.get('src', None)
-        _src = _src or prompt(u'Enter URL of repo with source code:')
-        self.src = _src
+        _path = os.path.join(self.path, 'src')
+        _src = src or self.src
+        self.src = _src or prompt(u'Enter URL of repo with source code:')
         with settings(warn_only=True):
-            result = run('git clone {0} {1}'.format(_src, _path))
+            result = run('git clone {0} {1}'.format(self.src, _path))
             if result.failed:
                 abort(red(result))
         print(green(u'"{src}" was clone.').format(**self.__dict__))
--- petrofab/teamocil.py
+++ petrofab/teamocil.py
@@ -6,6 +6,7 @@ import os
 # fabric.api
 from fabric.api import settings
 from fabric.colors import green, red
+from fabric.context_managers import cd
 from fabric.operations import prompt, run
 from fabric.utils import abort
 
@@ -27,44 +28,59 @@ class Teamocil(object):
     def __init__(self, path, name=None, filename=None, root=None, teamocil={}):
         super(Teamocil, self).__init__()
         self.path = path_abs(path)
-        self.name = name
-        self.filename = filename or teamocil.get('filename', '')
         self.env = teamocil
-        self.root = root
+        self.name = name or self.env.get('name', None)
+        self.filename = filename or self.env.get('filename', None)
+        self.root = root or self.env.get('root', None)
 
-    def init_layout(self, name=None):
-        """Replace examples patterns in specific teamocil config."""
-
-        _path = self.path
-        _name = name or self.name or self.env.get('name', None)
+    def _get_name(self, name):
+        _name = name or self.name
         _name = _name or prompt(u'Teamocil layout name:')
-        self.name = _name
-        layout = os.path.join(_path, self.filename)
+        return _name
+
+    def _get_filename(self, filename):
+        _filename = filename or self.filename
+        _filename = _filename or prompt(u'Teamocil layout filename:')
+        return _filename
+
+    def _get_root(self, root):
+        _root = root or self.root
+        _root = _root or prompt(u'Path to Teamocil root directory:')
+        return path_abs(_root)
+
+    def _get_layout(self, path, filename):
+        layout = os.path.join(path, filename)
         if not os.path.isfile(layout):
             abort(red(u'"%s" not exists!' % layout))
-        for line in fileinput.input(layout, inplace=True):
-            new = line.rstrip()
-            new = new.replace(u'<example>', _name)
-            new = new.replace(u'<Example>', _name.title())
-            new = new.replace(u'<example/path>', _path)
-            print(new)
+        return layout
+
+    def init_layout(self, name=None, filename=None):
+        """Replace examples patterns in specific teamocil config."""
+        self.name = self._get_name(name)
+        self.filename = self._get_filename(filename)
+        layout = self._get_layout(self.path, self.filename)
+        repls = (
+            (u'<example>', self.name),
+            (u'<Example>', self.name.title()),
+            (u'<example/path>', self.path),
+        )
+        with cd(self.path), settings(warn_only=True):
+            for repl in repls:
+                result = run('sed -i.bak -e \'s|{0}|{1}|g\' {file}'.format(
+                    *repl, file=layout))
+                if result.failed:
+                    abort(red(result))
         print(green(u'Teamocil layout in "{0}" was init.').format(layout))
 
-    def make_layout(self, root=None, name=None):
+    def make_layout(self, root=None, name=None, filename=None):
         """Make Teamocil layout in root directory."""
-        _path = self.path
-        _root = root or self.root or self.env.get('root', None)
-        _root = _root or prompt(u'Path to Teamocil root directory:')
-        self.root = path_abs(_root)
-        _name = name or self.name or self.env.get('name', None)
-        _name = _name or prompt(u'Teamocil layout name:')
-        self.name = _name
-        link = os.path.join(self.root, '%s.yml' % _name)
-        layout = os.path.join(_path, self.filename)
-        if not os.path.isfile(layout):
-            abort(red(u'"%s" not exists!' % layout))
+        self.root = self._get_root(root)
+        self.name = self._get_name(name)
+        self.filename = self._get_filename(filename)
+        link = os.path.join(self.root, '%s.yml' % self.name)
+        layout = self._get_layout(self.path, self.filename)
         with settings(warn_only=True):
             result = run('ln -s {0} {1}'.format(layout, link))
             if result.failed:
                 abort(red(result))
-        print(green(u'Teamocil layout "{0}" was create.').format(_name))
+        print(green(u'Teamocil layout "{0}" was create.').format(self.name))
